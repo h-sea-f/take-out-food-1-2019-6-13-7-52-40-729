2321bac7aeae7fc7ff51bf46a6fbc429277de041
h-sea-f <h_sea_f@163.com>
2019-07-10 22:55:35 +0800

    finish getItem,getBarCodeList,isAppointPromotion function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,34 @@
+function getItem(tag){
+	const allItems=loadAllItems();
+	let item;
+	allItems.forEach((oneItems)=>{
+		if(tag.split(" x ")[0]===oneItems.id){
+			item=oneItems;
+		}
+	});
+	return item;
+}
+
+function getBarCodeList(tags){
+	const barCodeList=tags.map((tag)=>{
+		return tag.(" x ")[0];
+	});
+	return barCodeList;
+}
+
+function isAppointPromotion(barCodeList){
+	const appointPromotion=loadPromotions()[1].items;
+	let flag=0;
+	barCodeList.forEach((barCode)=>{
+		appointPromotion.forEach((item)=>{
+			if(barCode===item) flag=1;
+		});
+	});
+	if(flag==0) return false;
+	else return true;
+}
+
 function bestCharge(selectedItems) {
-  return /*TODO*/;
+	
+  	return /*TODO*/;
 }
